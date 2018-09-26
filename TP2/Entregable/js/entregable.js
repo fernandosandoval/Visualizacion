5312a50b0db20c980a2e7d43085c2a16d1619a7a
Fernando Sandoval <fdsandoval@gmail.com>
2018-09-26 02:44:15 -0300

    Drag and drop de fichas en tablero

--- TP2/Entregable/js/entregable.js
+++ TP2/Entregable/js/entregable.js
@@ -11,96 +11,195 @@ var posy = 200;
 var click = false;
 var jugador1 = true;                   // inicia con turno para el jugador 1
 var jugador2 = false;
+var dragok;
+var startX;
+var startY;
 
-ctx.fillStyle="blue";
-ctx.fillRect(225,160,450,380);
+canvas.onmousedown = presionandoMouse;
+canvas.onmouseup = soltandoMouse;
+canvas.onmousemove = moviendoMouse;
 
-for (fila=1; fila<=7; fila++){
-    for (columna=1; columna<=6; columna++){
-          casillero = new Casillero (posx, posy,'white', 25);
-          posy += 60;
-          arregloCasilleros.push(casillero);
-          casillero.draw(ctx);
-        }
-    posy = 200;
-    posx+= 60;
-}
+dibujarTablero();
+dibujarFichas();
 
-posx = 30;
-posy = 170;
-for (fila=1; fila<=3; fila++){
-    for (columna=1; columna<=7; columna++){
-          ficha = new Ficha (posx, posy, 25,'yellow');
-          posy += 60;
-          arregloFichasJug1.push(ficha);
-          ficha.draw(ctx);
-          console.log("Se ha creado una ficha amarilla con posicion x=",posx," y=",posy);
-        }
-    posy = 170;
-    posx+= 60;
-}
+function dibujarTablero(){
+      ctx.fillStyle="blue";
+      ctx.fillRect(225,160,450,380);
 
-posx = 750;
-posy = 170;
-for (fila=1; fila<=3; fila++){
-    for (columna=1; columna<=7; columna++){
-          ficha = new Ficha (posx, posy, 25, 'red');
-          posy += 60;
-          arregloFichasJug2.push(ficha);
-          ficha.draw(ctx);
-          console.log("Se ha creado una ficha roja con posicion x=",posx," y=",posy);
-        }
-    posy = 170;
-    posx+= 60;
+      for (fila=1; fila<=7; fila++){
+          for (columna=1; columna<=6; columna++){
+                casillero = new Casillero (posx, posy,'white', 25);
+                posy += 60;
+                arregloCasilleros.push(casillero);
+                casillero.draw(ctx);
+              }
+          posy = 200;
+          posx+= 60;
+      }
 
 }
+
+
+function limpiar() {
+     ctx.clearRect(0, 0, 900, 600);
+}
+
+function dibujarFichas(){
+
+        posx = 30;
+        posy = 170;
+        for (fila=1; fila<=3; fila++){
+            for (columna=1; columna<=7; columna++){
+                  ficha = new Ficha (posx, posy, 25,'yellow');
+                  posy += 60;
+                  arregloFichasJug1.push(ficha);
+                  ficha.draw(ctx);
+                  console.log("Se ha creado una ficha amarilla con posicion x=",posx," y=",posy);
+                }
+            posy = 170;
+            posx+= 60;
+        }
+
+        posx = 750;
+        posy = 170;
+        for (fila=1; fila<=3; fila++){
+            for (columna=1; columna<=7; columna++){
+                  ficha = new Ficha (posx, posy, 25, 'red');
+                  posy += 60;
+                  arregloFichasJug2.push(ficha);
+                  ficha.draw(ctx);
+                  console.log("Se ha creado una ficha roja con posicion x=",posx," y=",posy);
+                }
+            posy = 170;
+            posx+= 60;
+
+        }
+}
+
 
 function isIntersect(point, circle) {
            return Math.sqrt((point.x-circle.posX) ** 2 + (point.y - circle.posY) ** 2) < circle.radio;
       }
 
-canvas.addEventListener('mousedown', (e) => {
-        var rect = canvas.getBoundingClientRect();
-        scaleX = canvas.width / rect.width,
-        scaleY = canvas.height / rect.height;
-        const pos = {
-          x: (e.clientX - rect.left) * scaleX,
-          y: (e.clientY - rect.top) * scaleY
-        };
-        arregloFichasJug1.forEach(circle => {
-          if (isIntersect(pos, circle)) {
-            console.log('ud esta presionando una ficha amarilla');
-          }
-        });
-        arregloFichasJug2.forEach(circle => {
-          if (isIntersect(pos, circle)) {
-            console.log('ud esta presionando una ficha roja');
-          }
-        });
-        arregloCasilleros.forEach(circle => {
-          if (isIntersect(pos, circle)) {
-            console.log('ud esta presionando un casillero');
+function presionandoMouse(e){
+
+              e.preventDefault();
+              e.stopPropagation();
+
+              // obtengo posicion actual del mouse
+              var rect = canvas.getBoundingClientRect();
+              scaleX = canvas.width / rect.width,
+              scaleY = canvas.height / rect.height;
+              const pos = {
+                x: (e.clientX - rect.left) * scaleX,
+                y: (e.clientY - rect.top) * scaleY
+              };
+
+              // verifico para cada ficha si estoy haciendo click adentro
+
+              dragok=false;
+              arregloFichasJug1.forEach(circle => {
+                if (isIntersect(pos, circle)) {
+                  console.log('ud esta presionando una ficha amarilla');
+                  dragok=true;
+                  circle.dragging=true;
+                }
+              });
+
+            //  dragok = false;
+              arregloFichasJug2.forEach(circle => {
+                if (isIntersect(pos, circle)) {
+                  console.log('ud esta presionando una ficha roja');
+                  dragok=true;
+                  circle.dragging=true;
+                }
+              });
+
+              // guardamos la posicion del mouse
+              startX = pos.x;
+              startY = pos.y;
           }
 
-        });
-      });
+function soltandoMouse(e){
+          e.preventDefault();
+          e.stopPropagation();
+
+              // ponemos todas las variables de drag en false
+          dragok = false;
+          for (var i=0; i<arregloFichasJug1.length; i++){
+                      arregloFichasJug1[i].dragging = false;
+                  }
+          for (var i=0; i<arregloFichasJug2.length; i++){
+                      arregloFichasJug2[i].dragging = false;
+                          }
+
+}
+
+function moviendoMouse(e){
+
+        if (dragok){
+          e.preventDefault();
+          e.stopPropagation();
+          var rect = canvas.getBoundingClientRect();
+          scaleX = canvas.width / rect.width,
+          scaleY = canvas.height / rect.height;
+          const pos = {
+            x: (e.clientX - rect.left) * scaleX,
+            y: (e.clientY - rect.top) * scaleY
+          };
+
+          // calculo la distancia que se movio el mouse
+
+          var dx = pos.x-startX;
+          var dy = pos.y-startY;
+
+          // muevo cada ficha que tenga dragging en true
+
+          for(var i=0; i<arregloFichasJug1.length; i++){
+              var f1 = arregloFichasJug1[i];
+              if(f1.dragging){
+                  f1.posX+=dx;
+                  f1.posY+=dy;
+                  redibujar(ctx);
+              }
+          }
+
+          for(var i=0; i<arregloFichasJug2.length; i++){
+              var f2 = arregloFichasJug2[i];
+              if(f2.dragging){
+                  f2.posX+=dx;
+                  f2.posY+=dy;
+                  redibujar(ctx);
+              }
+          }
 
 
-// canvas.addEventListener("mousedown",function(){
-//         presionando = true;
-//         if (jugador1)
-//          for (i=0; i<arregloFichasJug1.length; i++){
-//             if(arregloFichasJug1[i].isClicked())
-//                console.log("Ud esta presionando la ficha")
-//          }
-//       })
-//
-// canvas.addEventListener("mouseup",function(){
-//         presionando = false;
-//       })
-//
-// canvas.addEventListener("mousemove",function(event){
-//         if(presionando){
-//                        hacerDrag(event);
-//                        }
-// }
+
+          // seteo nueva posicion inicial
+          startX = pos.x;
+          startY = pos.y;
+
+        }
+    }
+
+function redibujar(ctx){
+  limpiar();
+  // redibujo fichas amarillas
+  for(var i=0; i<arregloFichasJug1.length; i++){
+      var f = arregloFichasJug1[i];
+      f.draw(ctx);
+  }
+  // redibujo fichas rojas
+  for(var i=0; i<arregloFichasJug2.length; i++){
+      var f = arregloFichasJug2[i];
+      f.draw(ctx);
+  }
+  // redibujo tablero
+  ctx.fillStyle="blue";
+  ctx.fillRect(225,160,450,380);
+  for(var i=0; i<arregloCasilleros.length; i++){
+      var f = arregloCasilleros[i];
+      f.draw(ctx);
+  }
+
+}
--- TP2/Entregable/js/ficha.js
+++ TP2/Entregable/js/ficha.js
@@ -5,6 +5,7 @@ class Ficha{
     		this.posY = y;
         this.radio = radio;
     		this.color = color;
+        let dragging = false;
     	}
 
     	draw(ctx) {
@@ -14,6 +15,14 @@ class Ficha{
     		ctx.fill();
     		ctx.closePath();
     	}
+
+      undraw(ctx){
+        ctx.beginPath();
+        ctx.arc(this.posX, this.posY, this.radio, 0, Math.PI*2, false);
+        ctx.clip();
+        ctx.clearRect(this.posX - this.radio - 1, this.posY - this.radio - 1,
+                      this.radio * 2 + 2, this.radio * 2 + 2);
+      }
 
 
 }
