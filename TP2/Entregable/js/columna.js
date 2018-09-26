3c25dd88f46f7e7e64eab3cbadcaacdc86ef2b29
Fernando Sandoval <fdsandoval@gmail.com>
2018-09-26 18:46:49 -0300

    Identificación de columnas en tablero y actualización de fichas disponibles

---
+++ TP2/Entregable/js/columna.js
@@ -0,0 +1,18 @@
+class Columna{
+
+    	constructor(x, y, width, height, name){
+    		this.posX = x;
+    		this.posY = y;
+    		this.width = width;
+        this.height = height;
+        this.name = name;
+      }
+
+
+    	draw(ctx) {
+        console.log("dibuja area sobre columna");
+    		ctx.fillRect(this.posX, this.posY, this.width, this.height);
+    	}
+
+
+}
--- TP2/Entregable/js/entregable.js
+++ TP2/Entregable/js/entregable.js
@@ -1,11 +1,12 @@
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 var fila = 0;
-var columna = 0;
+var col = 0;
 var cant = 0;
 var arregloFichasJug1 = [];
 var arregloFichasJug2 = [];
 var arregloCasilleros = [];
+var arregloColumnas = [];
 var posx = 270;
 var posy = 200;
 var click = false;
@@ -14,20 +15,29 @@ var jugador2 = false;
 var dragok;
 var startX;
 var startY;
+var minX;
+var maxX;
+var minY;
+var maxY;
+var columnaActual;
+var initX;
+var initY;
 
 canvas.onmousedown = presionandoMouse;
 canvas.onmouseup = soltandoMouse;
 canvas.onmousemove = moviendoMouse;
 
+
 dibujarTablero();
 dibujarFichas();
+dibujarColumnas();
 
 function dibujarTablero(){
-      ctx.fillStyle="blue";
+      ctx.fillStyle = "blue";
       ctx.fillRect(225,160,450,380);
 
       for (fila=1; fila<=7; fila++){
-          for (columna=1; columna<=6; columna++){
+          for (col=1; col<=6; col++){
                 casillero = new Casillero (posx, posy,'white', 25);
                 posy += 60;
                 arregloCasilleros.push(casillero);
@@ -36,6 +46,20 @@ function dibujarTablero(){
           posy = 200;
           posx+= 60;
       }
+
+}
+
+function dibujarColumnas(){
+      ctx.fillStyle = "white";
+      posx = 240;
+      posy = 100;
+      for (col=1; col<=7; col++){
+                columna = new Columna (posx, posy, 59, 60, col);
+      //          console.log(columna.name)
+                posx += 60;
+                arregloColumnas.push(columna);
+                columna.draw(ctx);
+              }
 
 }
 
@@ -49,7 +73,7 @@ function dibujarFichas(){
         posx = 30;
         posy = 170;
         for (fila=1; fila<=3; fila++){
-            for (columna=1; columna<=7; columna++){
+            for (col=1; col<=7; col++){
                   ficha = new Ficha (posx, posy, 25,'yellow');
                   posy += 60;
                   arregloFichasJug1.push(ficha);
@@ -63,7 +87,7 @@ function dibujarFichas(){
         posx = 750;
         posy = 170;
         for (fila=1; fila<=3; fila++){
-            for (columna=1; columna<=7; columna++){
+            for (col=1; col<=7; col++){
                   ficha = new Ficha (posx, posy, 25, 'red');
                   posy += 60;
                   arregloFichasJug2.push(ficha);
@@ -80,6 +104,23 @@ function dibujarFichas(){
 function isIntersect(point, circle) {
            return Math.sqrt((point.x-circle.posX) ** 2 + (point.y - circle.posY) ** 2) < circle.radio;
       }
+
+function isInsideColumn(point, column) {
+           minX = column.posX;
+           minY = column.posY;
+           maxX = column.posX + column.width;
+           maxY = column.posY + column.height;
+      //     console.log("xmin=",minX, " ymin=", minY, "xmax=", maxX, " ymax=", maxY, " px=",point.x, " py=", point.y);
+           if ((point.x >= minX) && (point.y >= minY) && (point.x <= maxX) && (point.y <=maxY)){
+    //          console.log("adentro");
+              return true;
+           }
+           else{
+      //        console.log("afuera");
+              return false;
+           }
+}
+
 
 function presionandoMouse(e){
 
@@ -101,6 +142,8 @@ function presionandoMouse(e){
               arregloFichasJug1.forEach(circle => {
                 if (isIntersect(pos, circle)) {
                   console.log('ud esta presionando una ficha amarilla');
+                  initX = pos.x;
+                  initY = pos.y;
                   dragok=true;
                   circle.dragging=true;
                 }
@@ -110,8 +153,16 @@ function presionandoMouse(e){
               arregloFichasJug2.forEach(circle => {
                 if (isIntersect(pos, circle)) {
                   console.log('ud esta presionando una ficha roja');
+                  initX = pos.x;
+                  initY = pos.y;
                   dragok=true;
                   circle.dragging=true;
+                }
+              });
+
+              arregloColumnas.forEach(column => {
+                if (isInsideColumn(pos, column)) {
+                  console.log('ud esta haciendo click sobre la columna ', column.name );
                 }
               });
 
@@ -120,10 +171,46 @@ function presionandoMouse(e){
               startY = pos.y;
           }
 
+function verificarUbicacionFicha(posActual){
+          arregloColumnas.forEach(column => {
+            if (isInsideColumn(posActual, column)) {
+              console.log('ud esta haciendo click sobre la columna ', column.name );
+              columnaActual = column.name;
+              console.log(columnaActual);
+            }
+            // else {
+            //   columnaActual = 0;
+            // }
+          });
+}
+
 function soltandoMouse(e){
           e.preventDefault();
           e.stopPropagation();
-
+          if (dragok==true) {
+                const posActual = {
+                  x: startX,
+                  y: startY
+                };
+                verificarUbicacionFicha(posActual);
+                console.log(columnaActual);
+                if (columnaActual != 0){
+                   console.log("puse la ficha en la columna ", columnaActual);
+                   arregloFichasJug1.pop();
+                   // redibujo area sobre las columnas
+                   ctx.clearRect (200,90,450,70);
+                   ctx.fillStyle = "white";
+                   posx = 240;
+                   posy = 100;
+                   for (col=1; col<arregloColumnas.length; col++){
+                             var c = arregloColumnas[col];
+                             c.draw(ctx);
+                             posx += 60;
+                             }
+                   columnaActual = 0;
+          //         actualizarTablero(columnaActual);
+                }
+          }
               // ponemos todas las variables de drag en false
           dragok = false;
           for (var i=0; i<arregloFichasJug1.length; i++){
@@ -173,7 +260,7 @@ function moviendoMouse(e){
               }
           }
 
-
+        //  if ()
 
           // seteo nueva posicion inicial
           startX = pos.x;
@@ -201,5 +288,10 @@ function redibujar(ctx){
       var f = arregloCasilleros[i];
       f.draw(ctx);
   }
+  for(var i=0; i<arregloCasilleros.length; i++){
+      var f = arregloCasilleros[i];
+      f.draw(ctx);
+  }
+
 
 }
