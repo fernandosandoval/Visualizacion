bd92044a3f5872a33d064bb8ca330e59f962ca3e
Fernando Sandoval <fdsandoval@gmail.com>
2018-11-30 16:15:20 -0300

    Agregado marco de pagina del juego

--- TP4/Entregable/juego.js
+++ TP4/Entregable/juego.js
@@ -1,149 +1,235 @@
-var avion = {
-	x: 500,
-	y: 500
-}
 
-var enemigo = [{x: 150, y: 20}, {x:250, y: 40}, {x:350, y:60}, {x: 500, y:80}, {x: 650, y: 60}, {x: 750, y: 40}, {x: 850, y: 20}]
-
-var otroenemigo = [{x: 425, y: 10},  {x:500, y:5}, {x: 575, y: 10}]
-
-var balas = [];
-
-var puntos = 0;
-
-
-function mostrarAvion(){
-	document.getElementById('avion').style['top'] = avion.y + "px";
-	document.getElementById('avion').style['left'] = avion.x + "px";
-}
-
-function mostrarAvionEnemigo(){
-	var output = '';
-	for(var i=0; i<enemigo.length; i++){
-		output += "<div class='enemigo1' style='top:"+enemigo[i].y+"px; left:"+enemigo[i].x+"px;'></div>"
+var hero = {
+			x: 300,
+			y: 500
 	}
-	for(var i=0; i<otroenemigo.length; i++){
-		output += "<div class='otroenemigo' style='top:"+otroenemigo[i].y+"px; left:"+otroenemigo[i].x+"px;'></div>"
-	}
-	document.getElementById('enemigos').innerHTML = output;
-}
 
-function mostrarBalas(){
-	var output = '';
-	for (var i=0; i<balas.length; i++){
-		output += "<div class='bala' style='top:"+balas[i].y+"px; left:"+balas[i].x+"px;'></div>"
-	}
-	document.getElementById('balas').innerHTML = output;
-}
+var enemies = [
+			{x:50, y:50},
+			{x:150, y:150},
+			{x:250, y:75},
+			{x:350, y:250},
+			{x:450, y:125},
+			{x:650, y:75},
+			{x:750, y:50},
+	];
 
-function moverBalas(){
-	for(var i=0; i<balas.length; i++){
-		balas[i].y -= 5;
+var specialenemies = [
+			{x:400, y: 1},
+			{x:100, y: 30},
+			{x:700, y: 80}
+	]
 
-		if(balas[i].y < 0){
-			balas[i] = balas[balas.length-1];
-			balas.pop();
-			console.log(balas);
-		}
-	}
-}
+var bullets = [];
+var score = 0;
+var explosion_i = 0;
 
-function mostrarPuntaje(){
-	document.getElementById('puntos').innerHTML = puntos;
-}
 
-function detectarColision(){
-	for(var i=0; i<balas.length; i++){
-		for(var j=0; j<enemigo.length; j++){
-
-			if(Math.abs(balas[i].x - enemigo[j].x) < 10 &&
-			Math.abs(balas[i].y - enemigo[j].y) < 10) {
-				console.log('bala', i, 'y enemigo', j, 'coinciden');
-				puntos += 20;
-				}
+	function displayEnemies(){
+			var output = '';
+			for(var i=0; i<enemies.length; i++){
+					output += "<div class='enemy1' style='top:"+enemies[i].y+"px; left:"+enemies[i].x+"px;'></div>";
 			}
-		for(var k=0; k<otroenemigo.length; k++){
+			document.getElementById('enemies').innerHTML = output;
+	}
 
-			if(Math.abs(balas[i].x - otroenemigo[k].x) < 10 &&
-			Math.abs(balas[i].y - otroenemigo[k].y) < 10) {
-				console.log('bala', i, 'y enemigo B', k, 'coinciden');
-				puntos += 50;
+	function displaySpecialEnemies(){
+			var output = '';
+			for(var i=0; i<specialenemies.length; i++){
+					output += "<div class='specialenemies1' style='top:"+specialenemies[i].y+"px; left:"+specialenemies[i].x+"px;'></div>";
 			}
-		}
+			document.getElementById('specialenemies').innerHTML = output;
 	}
-}
 
- function choqueAvion(){
-	 	for(var j=0; j<enemigo.length; j++){
 
- 		if(Math.abs(avion.x - enemigo[j].x) < 10 &&
-	 		Math.abs(avion.y - enemigo[j].y) < 10) {
-	 			console.log('avion y enemigo', j, 'chocaron');
-	 			puntos -= 1000;
-	 		}
-	 	}
-	 	for(var k=0; k<otroenemigo.length; k++){
-
-	 		if(Math.abs(avion.x - otroenemigo[k].x) < 10 &&
-	 		Math.abs(avion.y - otroenemigo[k].y) < 10) {
-	 			console.log('avion y otroenemigo', k, 'chocaron');
-	 			score -= 1500;
-	   	}
-	   }
-	 }
-
-function moverEnemigo(){
-	for(var i=0; i<enemigo.length; i++){
-		enemigo[i].y += 5;
-
-		if(enemigo[i].y > 540){
-			enemigo[i].y = 0;
-			enemigo[i].x = Math.random()*500;
-		}
+	function displayHero(){
+			document.getElementById('hero').style['top'] = hero.y + "px";
+			document.getElementById('hero').style['left'] = hero.x + "px";
 	}
-	for(var j=0; j<otroenemigo.length; j++){
-		otroenemigo[j].y += 5;
 
-		if(otroenemigo[j].y > 540){
-			otroenemigo[j].y = 0;
-			otroenemigo[j].x = Math.random()*500;
-		}
-	}
-}
 
-function juego(){
-		mostrarAvionEnemigo();
-		moverEnemigo();
-		mostrarAvion();
-		moverBalas();
-		mostrarBalas();
-		detectarColision();
-		mostrarPuntaje();
-		choqueAvion();
-}
+	function moveEnemies(){
+			for(var i=0; i<enemies.length; i++){
+					enemies[i].y += 5;
 
-setInterval(juego, 50);
+					if(enemies[i].y > 540){
+							enemies[i].y = 0;
+							enemies[i].x = Math.random()*700;
+					}
+			}
+	}
 
-document.onkeydown = function(a){
-	if(a.keyCode == 37){
-		avion.x -= 10;
+
+	function moveSpecialEnemies(){
+			for(var i=0; i<specialenemies.length; i++){
+					specialenemies[i].y += 7;
+
+					if(specialenemies[i].y > 540){
+							specialenemies[i].y = 0;
+							specialenemies[i].x = Math.random()*700;
+					}
+			}
 	}
-	else if(a.keyCode == 39){
-		avion.x += 10;
+
+
+	function displayBullets(){
+			var output = '';
+			for(var i=0; i<bullets.length; i++){
+					output += "<div class='bullets' style='top:"+bullets[i].y+"px; left:"+bullets[i].x+"px;'></div>";
+			}
+			document.getElementById('bullets').innerHTML = output;
 	}
-	if(a.keyCode == 38){
-		avion.y -= 10;
+
+	function moveBullets(){
+			for(var i=0; i<bullets.length; i++){
+					bullets[i].y -= 5;
+
+					if(bullets[i].y<0){
+							bullets[i] = bullets[bullets.length-1];
+							bullets.pop();
+					}
+			}
 	}
-	else if(a.keyCode == 40){
-		avion.y += 10;
+
+//    movimiento de avion
+	document.onkeydown = function(a) {
+			if(a.keyCode == 37){
+					hero.x -= 15;
+					
+			}else if(a.keyCode == 39){
+					hero.x +=15;
+			}else if(a.keyCode == 38){
+					hero.y -=15;
+			}else if(a.keyCode == 40){
+					hero.y +=15;
+			} else if(a.keyCode == 32){
+					bullets.push({x: hero.x+8, y: hero.y-15});
+			}
+			displayHero();
 	}
-	if(a.keyCode == 32){
-		balas.push({x: avion.x+8 , y: avion.y-15  });
-	  console.log(balas);
-		mostrarBalas();
+
+//    EXPLOSIONS
+	function createExplosion(){
+			enemies[i]
 	}
-	mostrarAvion();
-	console.log(avion);
-}
-mostrarAvionEnemigo();
-mostrarAvion();
+
+
+//    COLLISION DETECTION
+//    ENEMIES & BULLETS
+	function detectCollision(){
+			for(var i=0; i<bullets.length; i++){
+					for(var j=0; j<enemies.length; j++){
+							if( Math.abs(bullets[i].x - enemies[j].x) < 10 && Math.abs(bullets[i].y - enemies[j].y) < 10){
+									score += 10;
+									var mydiv = document.createElement("div");
+									mydiv.style.top = enemies[j].y+"px";
+									mydiv.style.left = enemies[j].x+"px";
+									mydiv.className = "explosions1";
+
+									document.getElementById('explosions').appendChild(mydiv);
+
+									setTimeout(function() {
+											mydiv.parentNode.removeChild(mydiv);
+									}, 800);
+									enemies.splice(j, 1);
+							}
+					}
+					for(var j=0; j<specialenemies.length; j++){
+							if( Math.abs(bullets[i].x - specialenemies[j].x) < 10 && Math.abs(bullets[i].y - specialenemies[j].y) < 10){
+									score += 10;
+									var mydiv = document.createElement("div");
+									mydiv.style.top = specialenemies[j].y+"px";
+									mydiv.style.left = specialenemies[j].x+"px";
+									mydiv.className = "explosions1";
+
+									document.getElementById('explosions').appendChild(mydiv);
+
+									setTimeout(function() {
+											mydiv.parentNode.removeChild(mydiv);
+									}, 800);
+									specialenemies.splice(j, 1);
+							}
+					}
+			}
+
+//        ENEMIES & HERO
+			for(var i=0; i<enemies.length; i++){
+					if(Math.abs(enemies[i].x - hero.x) < 10 && Math.abs(enemies[i].y - hero.y) < 10){
+							score = "GAME OVER";
+							var mydiv = document.createElement("div");
+							mydiv.style.top = enemies[i].y+"px";
+							mydiv.style.left = enemies[i].x+"px";
+							mydiv.className = "explosions1";
+
+							document.getElementById('explosions').appendChild(mydiv);
+
+							setTimeout(function() {
+									mydiv.parentNode.removeChild(mydiv);
+							}, 800);
+							enemies.splice(i, 1);
+							document.getElementById('hero').remove();
+							stopGame();
+					}
+			}
+
+			for(var i=0; i<specialenemies.length; i++){
+					if(Math.abs(specialenemies[i].x - hero.x) < 10 && Math.abs(specialenemies[i].y - hero.y) < 10){
+							score -= 500;
+							var mydiv = document.createElement("div");
+							mydiv.style.top = specialenemies[i].y+"px";
+							mydiv.style.left = specialenemies[i].x+"px";
+							mydiv.className = "explosions1";
+
+							document.getElementById('explosions').appendChild(mydiv);
+
+							setTimeout(function() {
+									mydiv.parentNode.removeChild(mydiv);
+							}, 800);
+							specialenemies.splice(i, 1);
+							document.getElementById('hero').remove();
+							stopGame();
+					}
+			}
+	}
+
+	function explosionRemoval(){
+			return;
+			if(explosions.length>0){
+					var elem = document.getElementById(explosions[explosions.length-1]);
+					console.log(explosions);
+					console.log(elem);
+					explosions.pop();
+					setTimeout(function(){ elem.remove(); }, 800);
+			}
+	}
+
+
+
+
+	function displayScore(){
+			document.getElementById('score').innerHTML = score;
+	}
+
+
+
+	function gameLoop(){
+			displayHero();
+			moveEnemies();
+			moveSpecialEnemies();
+			displayEnemies();
+			displaySpecialEnemies();
+			moveBullets();
+			displayBullets();
+			detectCollision();
+			displayScore();
+			explosionRemoval();
+	}
+
+
+	var myGame = setInterval(gameLoop, 30);
+	myGame();
+
+
+	function stopGame() {
+			clearInterval(myGame);
+	}
